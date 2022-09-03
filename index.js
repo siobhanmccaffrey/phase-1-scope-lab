3884151d02691aef2974f06ad8b6dd7424697979
SiobhanMcCaffrey <siobhanamccaffrey@gmail.com>
2022-09-03 17:56:17 -0400

    commit tree ef695a22fb90e01f06f6c69f45c1bb41e524f007

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'annie'
+
+changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'you'
+    return leastFavoriteCustomer
+}
